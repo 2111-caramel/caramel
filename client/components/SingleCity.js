6c68fb2b038445a318256a8308a44d27282dcc35
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-01-31 17:31:09 -0500

    pulled data from api

--- client/components/SingleCity.js
+++ client/components/SingleCity.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import { getWeather } from '../store/singleCity'
+import { getWeather, getCostOfLiving, getTransportation, getPollution } from '../store/singleCity'
 
 /**
  * COMPONENT
@@ -9,13 +9,18 @@ class SingleCity extends Component {
 
   componentDidMount(){
     this.props.loadWeather()
+    this.props.loadCostOfLiving()
+    this.props.loadTransportation()
+    this.props.loadPollution()
     console.log('state', this.state)
 
   }
   render(){
-    const weather = this.props.singleCity.month || [];
+    const weather = this.props.singleCity.weather.month || [];
+    const beer = this.props.singleCity.beer || 0;
+    const salary = this.props.singleCity.salary || 0;
+    const rent = this.props.singleCity.rent || 0;
     console.log('props', this.props)
-    console.log(weather, 'WEATHER')
     return (
       <div>
         <h3>New York Weather</h3>
@@ -24,8 +29,10 @@ class SingleCity extends Component {
           <h4>{month.name}</h4>
           <h6>{month.avgMinTemp_F} - {month.absMaxTemp_F}</h6>
         </div>
-
         )}
+        <h3>Beer {beer}</h3>
+        <h3>Salary {salary}</h3>
+        <h3>Rent {rent}</h3>
       </div>
     )
   }
@@ -42,7 +49,10 @@ const mapState = state => {
 
 const mapDispatch = (dispatch) => {
   return {
-    loadWeather: () => dispatch(getWeather())
+    loadWeather: () => dispatch(getWeather()),
+    loadCostOfLiving: () => dispatch(getCostOfLiving()),
+    loadTransportation: () => dispatch(getTransportation()),
+    loadPollution: () => dispatch(getPollution())
   }
 }
 
--- client/store/singleCity.js
+++ client/store/singleCity.js
@@ -2,40 +2,103 @@ import axios from 'axios'
 import history from '../history'
 
 const GET_WEATHER = 'GET_WEATHER'
+const GET_RENT = 'GET_RENT'
+const GET_BEER = 'GET_BEER'
+const GET_SALARY = 'GET_SALARY'
+const GET_TRANSPORTATION = 'GET_TRANSPORTATION'
+const GET_POLLUTION = 'GET_POLLUTION'
+
 
 
 /**
  * ACTION CREATORS
  */
 const _getCity = weather => ({type: GET_WEATHER, weather})
+const _getRent = rent => ({type: GET_RENT, rent})
+const _getBeer = beer => ({type: GET_BEER, beer})
+const _getSalary = salary => ({type: GET_SALARY, salary})
+const _getTransportation = transportation => ({type: GET_TRANSPORTATION, transportation})
+const _getPollution = pollution => ({type: GET_POLLUTION , pollution})
+
+
+
 
 /**
  * THUNK CREATORS
  */
 export const getWeather = () => async dispatch => {
-
   try{
-  //city format is + between words
+    //city format is + between words
     //city.replace(' ', '+')
-    console.log('HIIII')
     const res = await axios.get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=2d655123ca6d4c0094f172122222801&q=New+York&fx=no&cc=no&mca=yes&format=json`)
-    console.log('HELLO', res.data.data)
     return dispatch(_getCity(res.data.data.ClimateAverages[0]))
   } catch(error){
     console.log('get weather thunk error')
   }
+}
 
+export const getPollution = () => async dispatch => {
+  try{
+  //city format is + between words
+    //city.replace(' ', '+')
+    const res = await axios.get(`http://www.numbeo.com:8008/api/city_pollution?api_key=mjp50q4qjekq4w&city=New%20York,%20NY&country=United%20States`)
+    return dispatch(_getPollution(res.data))
+  } catch(error){
+    console.log('get pollution thunk error')
+  }
 }
 
 
+export const getTransportation = () => async dispatch => {
+  try{
+  //city format is space between words
+    const { data: res } = await axios.get(`http://www.numbeo.com:8008/api/city_traffic?api_key=mjp50q4qjekq4w&city=New%20York,%20NY&country=United%20States`)
+    dispatch(_getTransportation(res.primary_means_percentage_map))
+  } catch(error){
+    console.log('get transportation thunk error')
+  }
+}
+
+
+export const getCostOfLiving = () => async dispatch => {
+  try{
+  //city format is space between words
+    const { data: res } = await axios.get(`http://www.numbeo.com:8008/api/city_prices?api_key=mjp50q4qjekq4w&city=New%20York,%20NY&country=United%20States`)
+    dispatch(_getBeer(res.prices[3].average_price))
+    dispatch(_getRent(res.prices[21].average_price))
+    dispatch(_getSalary(res.prices[40].average_price))
+  } catch(error){
+    console.log('get cost of living thunk error')
+  }
+}
+
+
+let initialState = {
+  weather:{},
+  rent: 0,
+  beer: 0,
+  salary: 0,
+  transportation: {},
+  pollution: {}
+}
 
 /**
  * REDUCER
  */
-export default function singleCityReducer (state = {}, action){
+export default function singleCityReducer (state = initialState, action){
   switch (action.type) {
     case GET_WEATHER:
-      return action.weather
+      return {...state, weather: action.weather}
+    case GET_RENT:
+      return {...state, rent: action.rent}
+    case GET_BEER:
+      return {...state, beer: action.beer}
+    case GET_SALARY:
+      return {...state, salary: action.salary}
+    case GET_TRANSPORTATION:
+      return {...state, transportation: action.transportation}
+    case GET_POLLUTION:
+      return {...state, pollution: action.pollution}
     default:
       return state
   }
