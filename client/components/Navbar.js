f566df0cf349ad17f8712de828adc9e8f312e906
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-02-08 18:03:14 -0500

    routing works better

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -18,13 +18,23 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
               >
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <a className="nav-link-active" href="/home">
+                    <a className="nav-link-active" aria-current="page" href="/">
                       Home
                     </a>
                   </li>
                   <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="#">
+                      Take Survey
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link-active" aria-current="page" href="/compare">
+                      Compare Cities
+                    </a>
+                  </li>
+                  <li className="nav-item">
                     <a
-                      className="nav-link-active"
+                      className="nav-link-active" aria-current="page"
                       href="#"
                       onClick={handleClick}
                     >
@@ -32,7 +42,7 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link-active" href="/myInfo">
+                    <a className="nav-link-active" aria-current="page" href="/myInfo">
                       My Info
                     </a>
                   </li>
@@ -70,12 +80,12 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">
+                    <a className="nav-link" aria-current="page" href="#">
                       Take Survey
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">
+                    <a className="nav-link-active" aria-current="page" href="/compare">
                       Compare Cities
                     </a>
                   </li>
