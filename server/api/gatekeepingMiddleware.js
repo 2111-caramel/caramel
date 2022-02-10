d54c6b4d5891fe350a02761579fe240605276928
Montensia Banks <montensia@gmail.com>
2022-02-10 11:24:49 -0600

    Added gatekeeping middleware

---
+++ server/api/gatekeepingMiddleware.js
@@ -0,0 +1,13 @@
+const {
+  model: { User },
+} = require("../db");
+
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token);
+    req.user = user;
+  } catch (e) {
+    next(e);
+  }
+};
